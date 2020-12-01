a3f2b3de9307aea91f537d324d267a0e6403fefd
cerenbattal <cerenbattal97@gmail.com>
2020-12-01 20:51:07 +0300

    Updates

---
+++ precache-manifest.cb444dcf4418e7a42785a4ac3847bdce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc04e185b733742816cfe68c03f2cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee92cb27e98aaf4001c0",
+    "url": "/static/js/2.08ff9b4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.08ff9b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c025c5d83ff711a6d907",
+    "url": "/static/js/main.d5a2fe47.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  },
+  {
+    "revision": "b1ff4c87b082a797c311a7888826a629",
+    "url": "/static/media/pexels-photo-733857.b1ff4c87.jpeg"
+  }
+]);
